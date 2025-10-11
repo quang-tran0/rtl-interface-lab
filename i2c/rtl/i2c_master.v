2a62e447a81c667e32b91c23d5849c33a51f0b77
Quang Tran <quang.trd05@gmail.com>
2025-10-11 09:32:33 +0700

    feat add i2c write byte

--- i2c/rtl/i2c_master.v
+++ i2c/rtl/i2c_master.v
@@ -3,6 +3,7 @@ module i2c_master (
     input  wire reset_n,
     input  wire half_tick,
     input  wire start,
+    input  wire [7:0] data_in,
     output reg  scl,
     inout  wire sda,
     output reg  busy,
@@ -11,12 +12,16 @@ module i2c_master (
 
     reg [2:0] state;
     reg       sda_low;
+    reg [7:0] data_reg;
+    reg [2:0] bit_count;
 
     localparam IDLE         = 3'd0;
     localparam START        = 3'd1;
-    localparam STOP_LOW     = 3'd2;
-    localparam STOP_HIGH    = 3'd3;
-    localparam STOP_RELEASE = 3'd4;
+    localparam BIT_HIGH     = 3'd2;
+    localparam BIT_LOW      = 3'd3;
+    localparam STOP_LOW     = 3'd4;
+    localparam STOP_HIGH    = 3'd5;
+    localparam STOP_RELEASE = 3'd6;
 
     assign sda = sda_low ? 1'b0 : 1'bz;
 
@@ -25,6 +30,8 @@ module i2c_master (
             state   <= IDLE;
             scl     <= 1'b1;
             sda_low <= 1'b0;
+            data_reg <= 0;
+            bit_count <= 0;
             busy    <= 1'b0;
             done    <= 1'b0;
         end else begin
@@ -33,13 +40,32 @@ module i2c_master (
                 IDLE: if (start) begin
                     // start while scl is high
                     sda_low <= 1'b1;
+                    data_reg <= data_in;
                     busy    <= 1'b1;
                     state   <= START;
                 end
 
                 START: if (half_tick) begin
-                    scl   <= 1'b0;
-                    state <= STOP_LOW;
+                    scl       <= 1'b0;
+                    bit_count <= 3'd7;
+                    sda_low   <= ~data_reg[7];
+                    state     <= BIT_HIGH;
+                end
+
+                BIT_HIGH: if (half_tick) begin
+                    scl <= 1'b1;
+                    if (bit_count == 0)
+                        state <= STOP_LOW;
+                    else begin
+                        bit_count <= bit_count - 1'b1;
+                        state     <= BIT_LOW;
+                    end
+                end
+
+                BIT_LOW: if (half_tick) begin
+                    scl     <= 1'b0;
+                    sda_low <= ~data_reg[bit_count];
+                    state   <= BIT_HIGH;
                 end
 
                 STOP_LOW: if (half_tick) begin
